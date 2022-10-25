6a196ec4d5ae8c4de3f282421601741b532692ea
Kevin Mamaqi <kevinmamaqi@gmail.com>
2022-10-25 17:40:37 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
 import { Home } from './pages'
 import 'modern-normalize/modern-normalize.css'
-import { TestPage } from './pages/TestPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <Home /> */}
-    <TestPage />
+    <Home />
   </React.StrictMode>
 )
